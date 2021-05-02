c31f506511de87e2ff796cf16bbb0070d6ff1f99
Aslam Javed <Aslam Javed@DESKTOP-LRSG72U>
2021-05-01 17:05:51 -0700

    Question #01 Server Connection Message from Client to Server

---
+++ src/server/serverside.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Aslam Javed
+ */
+public class serverside {
+    public static void main(String[] args)  {
+        try {
+            ServerSocket ss = new ServerSocket(786);
+            while (true) {
+                 System.out.println("Going to Establish connection \n\n\n");
+            Socket s = ss.accept();//establishes connection   
+            System.out.println(" Connection Established \n\n\n");
+            
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            String str = (String) dis.readUTF();
+            System.out.println("message= Hello" + str);
+            
+            if(str.equalsIgnoreCase("|$|"))
+            {
+                ss.close();   
+                break;
+            }
+                
+            }
+           
+        
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+ 
+}
